f4d63a0c999b3dd44eb25d3a34956a74a16b2d95
DannyManrique <dannymanrique99@gmail.com>
2021-07-17 04:03:14 -0500

    Incluyendo Alumno.java

---
+++ ProyectoAED Actualizado/src/main/java/controller/Alumno.java
@@ -0,0 +1,22 @@
+package controller;
+public class Alumno extends Persona {
+	private int codigo;
+	public Alumno(String nombre, int edad, char sexo, double d, double e, int codigo2) {
+		super (nombre, edad, sexo, d, e);
+		this.codigo = codigo;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [codigo=" + codigo + "]";
+	}
+	
+}
